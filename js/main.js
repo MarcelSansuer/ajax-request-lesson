9748f370b66ded346b199868f31898a43f761151
Marcel Sansür <Marcel.Sansuer@hi-res.de>
2017-11-07 13:57:40 +0100

    add http and ajax request for forms

--- js/main.js
+++ js/main.js
@@ -1,12 +1,35 @@
 //listeners
-document.getElementById('btn').addEventListener('click',getName);
+document.getElementById('getForm').addEventListener('submit',getName);
+document.getElementById('postForm').addEventListener('submit',postName);
 
-function getName() {
+function postName(e) {
+    e.preventDefault();
+
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'process.php?name=Brad', true); 
+
+    var name = document.getElementById('name_Post').value;
+    var params = "name="+ name;
+
+    xhr.open('POST', 'process.php', true); 
+    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
     
     xhr.onload = function (){
-        console.log(this.responseText);
+        document.getElementById('user').innerHTML = xhr.responseText;
+    }
+
+    xhr.send(params);
+}
+
+function getName(e) {
+    e.preventDefault();
+    var xhr = new XMLHttpRequest();
+
+    var name = document.getElementById('name_GET').value;
+
+    xhr.open('GET', 'process.php?name='+name, true); 
+    
+    xhr.onload = function (){
+        document.getElementById('user').innerHTML = xhr.responseText;
     }
 
     xhr.send();
--- process.php
+++ process.php
@@ -1,7 +1,17 @@
+
+
 <?php
+//Connect to a database
+
+
 echo 'Processing ...';
 
 //check for GET variable
 if(isset($_GET['name'])){
     echo 'GET: your name is '.$_GET['name'];
+}
+
+//check for POST variables
+if(isset($_POST['name'])){
+    echo 'POST: your name is '.$_POST['name'];
 }
